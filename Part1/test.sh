677b100c22d609a589022a83a3f455d73ca159a6
Jingyan Xu <76541511+horatioxu1122@users.noreply.github.com>
2023-04-30 12:24:40 -0400

    Update test.sh

--- Part1/test.sh
+++ Part1/test.sh
@@ -3,8 +3,9 @@ source ../../env.sh
 ../../start.sh
 hadoop dfsadmin -safemode leave
 /usr/local/hadoop/bin/hdfs dfs -rm -r /P2/Part1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /P2/Part1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /P2/Part1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../p2data/train.csv /P2/Part1/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../p2data/test.csv /P2/Part1/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./P1.py hdfs://$SPARK_MASTER:9000/q3/input/
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./P1.py hdfs://$SPARK_MASTER:9000/P2/Part1/input/
 ../../stop.sh
